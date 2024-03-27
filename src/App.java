93b01577450ca6abec5a47ce128b18c893c68ed3
github user name <github email>
2024-03-27 19:45:27 -0400

    Item class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        //Item item1=new Item("Couch", " Furniture", "omar.fofana@quinnipiac.edu", 100.00);
+
+        //Item item2=new Item("Used Macbook","Technology","billy.hill@quinnipiac.edu" , 150.00);
+
+
+        //Item item3=new Item(" ",)
+
+
+
+
+
+
+
+
     }
 }
