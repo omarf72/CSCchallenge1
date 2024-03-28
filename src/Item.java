6bbbaba2432c429b97f2ef3507fd91383ca84d39
github user name <github email>
2024-03-27 21:05:28 -0400

    Added studentName variable and toString in item class

--- src/Item.java
+++ src/Item.java
@@ -2,12 +2,16 @@
 public class Item {
     private String name;
     private String category;
+    private String studentName;
     private double price;
 
-    public Item(String name, String category, double price) {
-        this.name = name;
-        this.category = category;
-        this.price = price;
+
+    public Item(String name,String category,String studentName,Double price)
+    {
+        this.name=name;
+        this.category=category;
+        this.studentName=studentName;
+        this.price=price;
     }
 
     // Getters and setters for name, category, and price
@@ -34,4 +38,21 @@ public class Item {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [name=" + name + ", category=" + category + ", studentName=" + studentName + ", price=" + price
+                + "]";
+    }
+
+
+    
 }
