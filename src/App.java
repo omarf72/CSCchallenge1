8167d42aa64c7d91f341391a6418626e845ea672
omarf72 <144395743+omarf72@users.noreply.github.com>
2024-04-08 12:44:45 -0400

    Buy option (#3)
    * Deleted extra file
    
    * Item class
    
    * Added studentName variable and toString in item class
    
    * added  showing the list of items for sale, allows user to see more information on an item, and allows user to add an item to the cart
    
    ---------
    
    Co-authored-by: github user name <github email>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void simulate(){
+        BuyOption buy= new BuyOption();
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println(" Choose an option:Buy,Sell,Cart,Logout : type Buy,Sell Cart or Logout");
+
+        String answer=scan.next();
+
+       System.out.println();
+
+        switch (answer) {
+            case "Buy":
+                System.out.println(" Items for sale: ");
+                System.out.println();
+                buy.displayList();
+                System.out.println("Do you what to see additional information of one of the items or buy an item : Type 'information' or 'buy' ");
+                String decision=scan.next();
+                if(decision.equals("information"))
+                {
+                    String itemChoice=scan.next();
+                    buy.selectItem(itemChoice);
+                }
+                if(decision.equals("buy"))
+                {
+                    System.out.println(" What would you like to buy");
+                    String choice=scan.next();
+                    buy.buyItem(choice);
+                    System.out.println("Would you like to go back to the main menu");
+                    String answer2=scan.next();
+                    if(answer2.equals("yes"))
+                    {
+                        simulate();
+                    }
+                   
+                }
+               
+                
+                break;
+
+            case "Logout":
+
+            
+
+            default:
+                break;
+        }
+
     }
-}
+    public static void main(String[] args) throws Exception {
+
+       simulate();
+
+        
+    }
+
+ }
